49cf0af45337198fa027ed89dd493b434cb5c239
wali.rahman89@yahoo.com <wali.rahman89@yahoo.com>
2017-11-12 15:54:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var customerName = 'bob';
 
 
 function upperCaseCustomerName() {
-  let customerName = customerName.toUpperCase;
+  var customerName = customerName.toUpperCase;
 }
 
 upperCaseCustomerName ();
